36a34447f212f9a1a33024ab68575719bec95eb8
Sushank Jadhav <sushank.jadhav.prdxn@gmail.com>
2017-06-14 15:11:09 +0530

    Test component imported in app file

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Fileupload from './file-uploader/';
+import TestFileUploader from './test-file-uploader/test-fileuploader.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Fileupload />
+        <TestFileUploader />
       </div>
     );
   }
